f1e714857141fab70cb4a9f06c88f63a38e9b726
sakagi <sakagi1121@outlook.jp>
2024-06-09 22:28:15 +0900

    resas/prefecturesを実装

---
+++ src/app/api/resas/prefectures/route.tsx
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET() {
+  const res = await axios.get(
+    "https://opendata.resas-portal.go.jp/api/v1/prefectures",
+    {
+      headers: { "X-API-KEY": process.env.API_KEY },
+    }
+  );
+  const data = await res.data;
+  return NextResponse.json(data);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Main from "../templates/Main";
+import axios from "axios";
 
-export default function Home() {
+export default async function Home() {
+  const res = await axios.get("http://127.0.0.1:3000/api/resas/prefectures");
+  console.log(res);
   return <Main />;
 }
