ebc2c5395de0eaa3da8184e27aca345334a7f87d
sakagi <sakagi1121@outlook.jp>
2024-06-09 23:25:08 +0900

    Mainのpropsを設定

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,3 +1,7 @@
-export default function Main() {
+import CheckField from "../components/CheckField";
+
+type Props = { prefectures: { prefCode: number; prefName: string }[] };
+
+export default function Main({ prefectures }: Props) {
   return <main></main>;
 }
