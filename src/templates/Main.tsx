eaffdf7ae7637ac28160d4f4a167dde3c64e7589
sakagi <sakagi1121@outlook.jp>
2024-06-11 16:38:42 +0900

    CheckField(デザイン)を実装

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -3,5 +3,16 @@ import CheckField from "../components/CheckField";
 type Props = { prefectures: { prefCode: number; prefName: string }[] };
 
 export default function Main({ prefectures }: Props) {
-  return <main></main>;
+  return (
+    <main>
+      <section>
+        <div className="w-5/6 m-auto mt-16">
+          <h2 className="text-2xl font-semibold border-l-4 border-gray-400 px-8 py-2 mb-8">
+            都道府県
+          </h2>
+          <CheckField prefectures={prefectures} />
+        </div>
+      </section>
+    </main>
+  );
 }
