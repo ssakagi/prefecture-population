1801822371b7c725e9435e4503a11e9c4ff17bda
sakagi <sakagi1121@outlook.jp>
2024-06-14 14:59:53 +0900

    不要なawaitを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default async function Home() {
   const res = await axios.get("http://127.0.0.1:3000/api/resas/prefectures");
-  const data = await res.data;
-  const prefectures = await data.result;
+  const data = res.data;
+  const prefectures = data.result;
   return <Main prefectures={prefectures} />;
 }
