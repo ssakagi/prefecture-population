d4623d2b65ff7b7456abcc600b74f0acc406aaf9
sakagi <sakagi1121@outlook.jp>
2024-06-14 14:53:23 +0900

    CheckField(ロジック)を実装

--- src/hooks/usePrefPopulation.tsx
+++ src/hooks/usePrefPopulation.tsx
@@ -9,4 +9,41 @@ export function usePrefPopulation() {
     }[]
   >([]);
   const [labelId, setLabelId] = useState(0);
+
+  async function handleClickCheck(
+    prefName: string,
+    prefCode: number,
+    checked: boolean
+  ) {
+    // チェックをつけた処理
+    if (checked) {
+      if (prefPopulation.some((value) => value.prefName === prefName)) return;
+      const res = await axios.get(
+        `/api/resas/population/composition/perYear?prefCode=${prefCode}`,
+        {
+          headers: { "X-API-KEY": process.env.API_KEY },
+        }
+      );
+      const data = res.data;
+      setPrefPopulation([
+        ...prefPopulation,
+        {
+          prefName: prefName,
+          data: data.result.data,
+        },
+      ]);
+    }
+    // チェックを外した処理
+    else {
+      setPrefPopulation(
+        prefPopulation.filter((value) => value.prefName !== prefName)
+      );
+    }
+  }
+
+  const populationdata = prefPopulation.map((value) => ({
+    prefName: value.prefName,
+    data: value.data[labelId],
+  }));
+  return { populationdata, handleClickCheck };
 }
